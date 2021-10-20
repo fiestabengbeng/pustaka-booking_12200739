2f1f44a473de24916f5391116f0c8ad5b4ea8f74
fiestabengbeng <ekadevienprasetyo@gmail.com>
2021-10-20 14:38:44 +0700

    Tugas Pertemuan 4

--- app/Database/Seeds/PenggunaSeeder_12200739.php
+++ app/Database/Seeds/PenggunaSeeder_12200739.php
@@ -19,7 +19,7 @@ class PenggunaSeeder extends Seeder
                 'password'  => md5('23456')
             ],
             [
-                'nama'      => 'marketing',
+                'nama'      => '12200739',
                 'password'  => md5('Eka Devien Prassetyo')
             ]
         ];
